ccc6f8891bd3dd679a0819d29a081c3a7efc8b5d
Ilhyo Sung <sih8189@gmail.com>
2020-04-10 15:43:47 -0400

    applied material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 
+import "./App.css";
+
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+
+import { withStyles } from "@material-ui/core/styles";
+
 import Customer from "./components/customer.component";
 
-import "./App.css";
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const customers = [
   {
@@ -17,7 +37,7 @@ const customers = [
     id: 2,
     image: "https://placeimg.com/64/64/2",
     name: "Sehee",
-    birthday: "860000",
+    birthday: "860209",
     gender: "Female",
     job: "Interior Decorator",
   },
@@ -31,24 +51,41 @@ const customers = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      {customers.map((customer) => {
-        return (
-          <Customer
-            key={customer.id}
-            id={customer.id}
-            image={customer.image}
-            name={customer.name}
-            birthday={customer.birthday}
-            gender={customer.gender}
-            job={customer.job}
-          />
-        );
-      })}
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Image</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Birthday</TableCell>
+              <TableCell>Gender</TableCell>
+              <TableCell>Image</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((customer) => {
+              return (
+                <Customer
+                  key={customer.id}
+                  id={customer.id}
+                  image={customer.image}
+                  name={customer.name}
+                  birthday={customer.birthday}
+                  gender={customer.gender}
+                  job={customer.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/customer.component.js
+++ src/components/customer.component.js
@@ -1,40 +1,21 @@
 import React from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 class Customer extends React.Component {
   render() {
     const { id, image, name, birthday, gender, job } = this.props;
     return (
-      <div>
-        <CustomerProfile id={id} image={image} name={name} />
-        <CustomerInfo birthday={birthday} gender={gender} job={job} />
-      </div>
-    );
-  }
-}
-
-class CustomerProfile extends React.Component {
-  render() {
-    const { id, name, image } = this.props;
-    return (
-      <div>
-        <img src={image} alt="profile" />
-        <h2>
-          {name}({id})
-        </h2>
-      </div>
-    );
-  }
-}
-
-class CustomerInfo extends React.Component {
-  render() {
-    const { birthday, gender, job } = this.props;
-    return (
-      <div>
-        <p>{birthday}</p>
-        <p>{gender}</p>
-        <p>{job}</p>
-      </div>
+      <TableRow>
+        <TableCell>{id}</TableCell>
+        <TableCell>
+          <img src={image} alt="profile" />
+        </TableCell>
+        <TableCell>{name}</TableCell>
+        <TableCell>{birthday}</TableCell>
+        <TableCell>{gender}</TableCell>
+        <TableCell>{job}</TableCell>
+      </TableRow>
     );
   }
 }
