7d60408a1fa5184fd2a93fe17e956cc57eab07bd
Ilhyo Sung <sih8189@gmail.com>
2020-04-20 20:14:15 -0400

    fixed error

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
